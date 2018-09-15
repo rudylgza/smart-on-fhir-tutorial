3e19942aeb9b0ee631b1a1009d05f05ee994847c
rudylgza <rodolfo_lopez@msn.com>
2018-09-15 14:06:03 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -12,7 +12,7 @@
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
+                    type: 'Inpatient',
                     query: {
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
@@ -87,7 +87,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
-      hrate:{value: ''};
+      hrate:{value: ''},
     };
   }
 
