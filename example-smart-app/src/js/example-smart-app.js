5100a3503232f5c6cdf30a3a56d912a9713785da
rudylgza <rodolfo_lopez@msn.com>
2018-09-16 08:25:10 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -17,7 +17,8 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
+                              'http://loinc.org|8867-4']
                       }
                     }
                   });
@@ -41,6 +42,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+          var hrate = byCodes('8867-4'); 
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -59,6 +61,8 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
+          
+          p.hrate = hrate;
 
           ret.resolve(p);
         });
@@ -83,6 +87,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+      hrate: {value: ''},
     };
   }
 
@@ -126,6 +131,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+    $('#hrate').html(p.hrate);
   };
 
 })(window);
